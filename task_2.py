b7851cb4ff9961104202337d430fddc8c9b357ab
Paul Foo <dev.paul.foo@gmail.com>
2016-06-01 12:39:31 +1000

    Probability dictionaries complete

--- task_2.py
+++ task_2.py
@@ -68,23 +68,35 @@ a0 = (train_data[:, [0, 15]]).astype(int)
 a_totals = {}
 a_value_count = {}
 p_cond = {}
-p_cond_continuous = {}
+mu_sigma_cont = {}
 
 class_counter = 0
-for a in attribute_type:
-    entry_name_prob_1 = "a" + str(class_counter) + "_" + "1" + "_"
-    entry_name_prob_2 = "a" + str(class_counter) + "_" + "2" + "_"
-    entry_name_totals = "a" + str(class_counter) + "_"
-    for i in xrange(1, a + 1):
-        totals_entry = entry_name_totals + str(i)
-        cond_prob_1 = entry_name_prob_1 + str(i)
-        cond_prob_2 = entry_name_prob_2 + str(i)
-        a_totals[totals_entry] = 0
-        a_value_count[cond_prob_1] = 0
-        a_value_count[cond_prob_2] = 0
-        p_cond[cond_prob_1] = 0
-        p_cond[cond_prob_2] = 0
-        # print totals_entry
+for type in attribute_type:
+    if type != -1:
+        entry_name_prob_1 = "a" + str(class_counter) + "_" + "1" + "_"
+        entry_name_prob_2 = "a" + str(class_counter) + "_" + "2" + "_"
+        entry_name_totals = "a" + str(class_counter) + "_"
+        for i in xrange(1, type + 1):
+            totals_entry = entry_name_totals + str(i)
+            cond_prob_1 = entry_name_prob_1 + str(i)
+            cond_prob_2 = entry_name_prob_2 + str(i)
+            a_totals[totals_entry] = 0
+            a_value_count[cond_prob_1] = 0
+            a_value_count[cond_prob_2] = 0
+            p_cond[cond_prob_1] = 0
+            p_cond[cond_prob_2] = 0
+            # print totals_entry
+    else:
+        entry_name_cont_mu1 = "a" + str(class_counter) + "_" + "1" + "_" + "mu"
+        entry_name_cont_sigma1 = "a" + str(class_counter) + "_" + "1" + "_" + "sigma"
+        entry_name_cont_mu2 = "a" + str(class_counter) + "_" + "2" + "_" + "mu"
+        entry_name_cont_sigma2 = "a" + str(class_counter) + "_" + "2" + "_" + "sigma"
+        mu_sigma_cont[entry_name_cont_mu1] = 0
+        mu_sigma_cont[entry_name_cont_mu2] = 0
+        mu_sigma_cont[entry_name_cont_sigma1] = 0
+        mu_sigma_cont[entry_name_cont_sigma2] = 0
+
+
     class_counter += 1
 
 # Populate Dictionaries Values -----------------------------------------------------------------------------------------
@@ -127,6 +139,51 @@ for entry in p_cond:
     prob_a_given_class = float(a_occurrence)/float(a_total)
     p_cond[entry] = prob_a_given_class
 
+
+# Populate mu sigma dictionary for continuous values -------------------------------------------------------------------
+
+# TODO Testing data from mu sigma cont
+
+a1_1_vals = []
+a1_2_vals = []
+
+for i in xrange(train_data.shape[1]):
+    if attribute_type[i] == -1:
+        key_mu_1 = "a" + str(i) + "_1" + "_mu"
+        key_mu_2 = "a" + str(i) + "_2" + "_mu"
+        key_sigma_1 = "a" + str(i) + "_1" + "_sigma"
+        key_sigma_2 = "a" + str(i) + "_2" + "_sigma"
+
+
+        column = train_data[:, [i, 15]]
+        c1_data = np.array([])
+        c2_data = np.array([])
+
+        for row in column:
+            if row[1] == 1:
+                c1_data = np.append(c1_data, row[0])
+                # if i == 1:
+                #     a1_1_vals.append(row[0])
+            else:
+                c2_data = np.append(c2_data, row[0])
+                # if i == 1:
+                #     a1_2_vals.append(row[0])
+        mu1 = np.mean(c1_data)
+        sigma1 = np.std(c1_data)
+        mu2 = np.mean(c2_data)
+        sigma2 = np.std(c2_data)
+
+        mu_sigma_cont[key_mu_1] = mu1
+        mu_sigma_cont[key_mu_2] = mu2
+        mu_sigma_cont[key_sigma_1] = sigma1
+        mu_sigma_cont[key_sigma_2] = sigma2
+
+# Test mu sigma values -------------------------------------------------------------------------------------------------
+
+
+# Process continuous data ----------------------------------------------------------------------------------------------
+
+# Garbage
 # mean_a1 = np.mean(train_data[:, 1])
 # stdev_a1 = np.std(train_data[:, 1])
 #
@@ -148,7 +205,15 @@ for entry in p_cond:
 # plt.plot(x, p, 'k', linewidth=2)
 # plt.show()
 
-
-# Process continuous data ----------------------------------------------------------------------------------------------
-
-
+# a1_max = math.ceil(max(a1_1_vals))
+# a1_min = math.floor(min(a1_1_vals))
+# x = np.linspace(a1_min, a1_max, 100)
+# mean_a1 = np.mean(a1_1_vals)
+# std_a1 = np.std(a1_1_vals)
+# print mean_a1, mu_sigma_cont["a1_1_mu"]
+# print std_a1, mu_sigma_cont["a1_1_sigma"]
+# p_a1 = norm.pdf(x, mean_a1, std_a1)
+# plt.hist(a1_1_vals, bins=25, normed=True, alpha=0.6)
+# plt.hist(a1_2_vals, bins=25, normed=True, alpha=0.6, color="green")
+# plt.plot(x, p_a1, 'k')
+# plt.show()
